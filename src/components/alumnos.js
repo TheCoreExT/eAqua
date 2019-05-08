dd0e5a24099765d1525dfe9070715591d139b2ed
Alan Alvarez Sánchez <alan@Air.local>
2019-05-08 18:19:26 -0500

    Conectar base de datos

--- src/components/alumnos.js
+++ src/components/alumnos.js
@@ -1,5 +1,26 @@
 import React,{Component} from 'react';
+import {Client}  from 'pg-native';
 
+const connectionData={
+    user: 'alan',
+    host: '',
+    database: 'eaqua',
+    password: '',
+    port: 5432
+}
+
+const client  = new Client(connectionData);
+
+client.connect();
+
+client.query('select * from alumno')
+    .then(response => {
+        console.log(response.rows)
+        client.end()
+    })
+    .catch(err => {
+        client.end()
+    })
 
 export default class Alumnos extends Component {
 
