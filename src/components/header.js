6549fbde3c6a6ad72565466816eb4edd4d6eebed
Alan Alvarez Sánchez <alan@Air.local>
2019-05-07 20:58:32 -0500

    archivo css de componente header

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,15 @@
 import React,{Component} from 'react';
-import { whileStatement } from '@babel/types';
+import './header.css';
 
 export default class Header extends Component {
 
     render() {
 
-        var div ={
-            backgroundColor: '#45B8E2'
-            
-        }
-
-        
+                
         return (
 
-            <div style={div} className="header">
-                <img src="/img/logo.png" alt="logo" height="50px"/>
+            <div className="header">
+                <img className="logo" src="/img/logo.png" alt="logo" height="50px"/>
             </div>
         );
     }
