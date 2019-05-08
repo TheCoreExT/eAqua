88c272bd19b975ef4d24684038c83702933e58cf
Alan Alvarez Sánchez <alan@Air.local>
2019-05-08 18:26:58 -0500

    Remove pg-native

--- src/components/alumnos.js
+++ src/components/alumnos.js
@@ -1,9 +1,9 @@
 import React,{Component} from 'react';
-import {Client}  from 'pg-native';
+const { Client } = require('pg')
 
 const connectionData={
     user: 'alan',
-    host: '',
+    host: 'localhost:3000',
     database: 'eaqua',
     password: '',
     port: 5432
