e6de67bde14264262311b2662b7e7f76671d5ed4
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-07 20:03:37 -0500

    completado base de datos script

--- eaqua.sql
+++ eaqua.sql
@@ -1,13 +1,55 @@
+DROP TABLE IF EXISTS pago_alumno;
+DROP TABLE IF EXISTS pago_instructor;
+DROP TABLE IF EXISTS clase_alumno;
+DROP TABLE IF EXISTS clase;
+DROP TABLE IF EXISTS instructor;
+DROP TABLE IF EXISTS alumno;
+
 CREATE TABLE alumno (
-    alumno_id INT NOT NULL,
+    alumno_id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     password VARCHAR(100) NOT NULL,
     telefono VARCHAR(20) NOT NULL,
     correo VARCHAR(30) NOT NULL,
-    estatura NUMERIC(2,2) NOT NULL,
-    peso NUMERIC(3,2) NOT NULL,
+    estatura NUMERIC(10,2) NOT NULL,
+    peso NUMERIC(10,2) NOT NULL,
     seguro VARCHAR(30) NOT NULL,
     tipo_sangre VARCHAR(10) NOT NULL,
     alergias VARCHAR(100) NOT NULL,
     otro_padecimiento VARCHAR(100) NOT NULL
- );
+);
+
+CREATE TABLE pago_alumno (
+    pago_alumno_id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    alumno_id INT NOT NULL REFERENCES alumno(alumno_id),
+    fecha DATE NOT NULL,
+    monto NUMERIC(10,2) NOT NULL
+);
+
+CREATE TABLE instructor (
+    instructor_id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    correo VARCHAR(30) NOT NULL,
+    clabe VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE pago_instructor (
+    pago_instructor_id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    instructor_id INT NOT NULL REFERENCES instructor(instructor_id),
+    fecha DATE NOT NULL,
+    monto NUMERIC(10,2) NOT NULL
+);
+
+CREATE TABLE clase (
+    clase_id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    dia VARCHAR(30) NOT NULL,
+    hora TIME NOT NULL,
+    instructor_id INT NOT NULL REFERENCES instructor(instructor_id)
+);
+
+CREATE TABLE clase_alumno (
+    clase_id INT NOT NULL REFERENCES clase(clase_id),
+    alumno_id INT NOT NULL REFERENCES alumno(alumno_id),
+    PRIMARY KEY (clase_id, alumno_id)
+);
