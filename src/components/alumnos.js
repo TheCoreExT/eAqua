88bd27f481cad5d7d2bd23e4f7a59e2c7c386c44
Alan Alvarez Sánchez <alan@Air.local>
2019-05-08 19:04:34 -0500

    x

--- src/components/alumnos.js
+++ src/components/alumnos.js
@@ -1,26 +1,4 @@
 import React,{Component} from 'react';
-const { Client } = require('pg')
-
-const connectionData={
-    user: 'alan',
-    host: 'localhost:3000',
-    database: 'eaqua',
-    password: '',
-    port: 5432
-}
-
-const client  = new Client(connectionData);
-
-client.connect();
-
-client.query('select * from alumno')
-    .then(response => {
-        console.log(response.rows)
-        client.end()
-    })
-    .catch(err => {
-        client.end()
-    })
 
 export default class Alumnos extends Component {
 
