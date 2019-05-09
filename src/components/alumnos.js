f47ba2c139070ca93f2bad317d0962b2a9164c04
Alan Alvarez Sánchez <alan@Air.local>
2019-05-08 20:17:52 -0500

    Nuevo archivo de base de datos

--- src/components/alumnos.js
+++ src/components/alumnos.js
@@ -1,29 +1,4 @@
 import React,{Component} from 'react';
-<<<<<<< HEAD
-=======
-const { Client } = require('pg')
-
-const connectionData={
-    user: 'alan',
-    host: 'localhost:3000',
-    database: 'eaqua',
-    password: '',
-    port: 5432
-}
-
-const client  = new Client(connectionData);
-
-client.connect();
-
-client.query('select * from alumno')
-    .then(response => {
-        console.log(response.rows)
-        client.end()
-    })
-    .catch(err => {
-        client.end()
-    })
->>>>>>> d9892cc3375e44898411a25d25a482c1ab68644d
 
 export default class Alumnos extends Component {
 
