3f0814db3322e521b99138cbc44212ce0c25260c
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-09 09:58:48 -0500

    ultima version que si trabaja

--- server/react-backend/routes/index.js
+++ server/react-backend/routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var mongo = require('mongodb')
-var MongoClient = require('mongodb').MongoClient;
-
-var url = "mongodb://localhost:27018/test";
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
