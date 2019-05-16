66e407a75913921f77c2de2d8d40fc95c762aa9f
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-15 20:28:58 -0500

    se agregaron los archivos de add alumno y clases

---
+++ src/components/addAlumno.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './addAlumno.css'
+
+export default class AddAlumno extends Component {
+
+    render() {
+        return (
+          <div className="AddAlumno">
+            
+          </div>
+        );
+    }
+}
---
+++ src/components/addClases.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import './addClases.css'
+
+export default class AddClases extends Component {
+
+    render() {
+        return (
+          <div className="AddClases">
+            
+          </div>
+        );
+    }
+}
