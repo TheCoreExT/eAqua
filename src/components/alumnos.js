34246e0e79889aa474c1c802b76b442c55193425
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-19 09:49:43 -0500

    agregue el boton mas informacion a alumnos

--- src/components/alumnos.js
+++ src/components/alumnos.js
@@ -21,9 +21,20 @@ export default class Alumnos extends Component {
                 <h2>Alumnos</h2>
                 <Link to="/addAlumno">Agregar Alumno</Link>
                 <table>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Teléfono</th>
+                        <th>Correo</th>
+                        <th>Estatura</th>
+                        <th>Peso</th>
+                        <th>Seguro</th>
+                        <th>tipo_sangre</th>
+                        <th>Otro</th>                        
+                    </tr>
                   {this.state.alumnos.map(alumno => (
 
                   <tr key={alumno.id}>
+                    
                     <td>{alumno.nombre}</td>
                     <td>{alumno.telefono}</td>
                     <td>{alumno.correo}</td>
@@ -40,10 +51,15 @@ export default class Alumnos extends Component {
                       </form>
 
                       </td>
-                  </tr>
+                      <td>
+                        <Link to={"/alumnos/" + alumno.id}>
 
-                
-              ))}
+                         <button>Más Información</button>
+                        </Link>
+                      </td>
+                    
+                     </tr>
+                    ))}
                 </table>
             </div>
         );
