a491b758f9dbf05a20f941020d9d73be2c88520a
marvilchis1 <ej.everlake@gmail.com>
2019-05-20 03:27:33 -0500

    Agrege barra de busqueda, no funcional

--- src/components/alumnos.js
+++ src/components/alumnos.js
@@ -19,28 +19,35 @@ export default class Alumnos extends Component {
         return (
             <div className="Alumnos">
                 <h2>Alumnos</h2>
-                <div className="AddAlumno">
-                    <Link to="/addAlumno">Agregar Alumno</Link>
-                </div>
 
-                <table>
-                    <tr>
-                        <th scope="col" class="Nombre" > Nombre </th>
-                        {/*<th scope="col" class="Clase" > Clase </th> */}
-                        <th scope="col" class="Telefono"> Teléfono </th>
-                        <th scope="col" class="Correo"> Correo </th>
-                        {/*
-                        <th scope="col" class="Estatura"> Estatura</th>
-                        <th scope="col" class="Peso"> Peso</th>
-                        <th scope="col" class="Seguro"> Seguro</th>
-                        <th scope="col" class="Tipo_Sangre"> Tipo Sangre</th>
-                        <th scope="col" class="Otros"> Otros</th>
-                        */}   
-                        <th scope="col" class="Detalles"> </th>
-                        <th scope="col" class="Editar"> </th>
-                        <th scope="col" class="Eliminar"> </th>
-                    </tr>
-                    {this.state.alumnos.map(alumno => (
+          
+            <div class="Buscar">
+                <input type="search" id="search" placeholder="Buscar" />
+            </div>
+            
+
+            <div className="AddAlumno">
+                <Link to="/addAlumno">Agregar Alumno</Link>
+            </div>
+
+            <table>
+                <tr>
+                    <th scope="col" class="Nombre" > Nombre </th>
+                    {/*<th scope="col" class="Clase" > Clase </th> */}
+                    <th scope="col" class="Telefono"> Teléfono </th>
+                    <th scope="col" class="Correo"> Correo </th>
+                    {/*
+                    <th scope="col" class="Estatura"> Estatura</th>
+                    <th scope="col" class="Peso"> Peso</th>
+                    <th scope="col" class="Seguro"> Seguro</th>
+                    <th scope="col" class="Tipo_Sangre"> Tipo Sangre</th>
+                    <th scope="col" class="Otros"> Otros</th>
+                    */}   
+                    <th scope="col" class="Detalles"> </th>
+                    <th scope="col" class="Editar"> </th>
+                    <th scope="col" class="Eliminar"> </th>
+                </tr>
+                {this.state.alumnos.map(alumno => (
                     <tr key={alumno.id}>
                         <td>{alumno.nombre}</td>
                         <td>{alumno.telefono}</td>
@@ -70,7 +77,7 @@ export default class Alumnos extends Component {
                         </td>
 
                     </tr>
-                    ))}
+                ))}
                 </table>
             </div>
         );
