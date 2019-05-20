4f4197ddb9033d3ff5a306079520b4780eaf2d06
Alan Alvarez Sánchez <alan@Air-2.local>
2019-05-20 09:23:31 -0500

    InfoInstructor

--- src/components/infoClase.js
+++ src/components/infoClase.js
@@ -2,23 +2,22 @@ import React,{Component} from 'react';
 
 export default class InfoClase extends Component {
 
-                state ={data : "" }
+  state ={data : "" }
 
-                 componentDidMount() {
-                   var url =
-                     "/clases/infoClase" + this.props.clase_id;
-                   fetch(url)
-                   .then(res => res.json())
-                  .then(data =>
-                      this.setState({ data })
-                   );
-                 }
+  componentDidMount() {
+    var url = "/clases/infoClase" +           this.props.clase_id;
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>
+      this.setState({ data })
+      );
+  }
 
-                 render() {
-                   return <div className="infoClase">
+  render() {
+    return <div className="infoClase">
 
-                       <h2>Clase de las {this.state.data.hora}hrs.</h2>
+      <h2>Clase de las {this.state.data.hora}hrs.</h2>
 
-                   </div>;
-                 }
-               }
+      </div>;
+  }
+}
