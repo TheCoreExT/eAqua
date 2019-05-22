1d941476e1e59d3f8dfe5e3096b6d24fb8fceb37
marvilchis1 <ej.everlake@gmail.com>
2019-05-21 19:54:51 -0500

    editando la vista anadir clase

--- src/components/addClases.js
+++ src/components/addClases.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import './css/addClases.css';
 
 export default class AddClases extends Component {
 
@@ -16,16 +17,24 @@ export default class AddClases extends Component {
         return (
           <div className="AddClases">
             <form action="/clases" method="post">
-              <input type="time" placeholder="Hora Inicial" name="hora_inicial" />
-              <input type="time" placeholder="Hora Final" name="hora_final" />
-              <input type="text" placeholder="Dia" name="dia"/>
-              <select name="instructor_id">
-                {this.state.instructores.map(instructor => (
-                  <option value={instructor.id}  >{instructor.nombre}</option>
-                ))}
-              </select>
+            <h2> Nueva clase </h2>
+              <div className="Hub">
+                <input type="time" placeholder="Hora Inicial" name="hora_inicial" />
+                <input type="time" placeholder="Hora Final" name="hora_final" />
+                <input type="text" placeholder="Dia" name="dia"/>
+                <br></br>
+                <select name="instructor_id">
+                  {this.state.instructores.map(instructor => (
+                    <option value={instructor.id}  >{instructor.nombre}</option>
+                  ))}
+                </select>
+              </div>
+              
+              <div className="SubmitClase">
+                <input type="submit" value="submit"/>
+              </div>
 
-              <input type="submit" value="submit"/>
+
             </form>
           </div>
         );
