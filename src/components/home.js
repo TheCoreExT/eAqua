110a4d5b8f88d4ef8b4208574bac12df6767580a
marvilchis1 <ej.everlake@gmail.com>
2019-05-26 23:42:41 -0500

    Home creado

--- src/components/home.js
+++ src/components/home.js
@@ -6,12 +6,38 @@ export default class Home extends Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <div className="Home">
-                        <h2>Home</h2>
+            <div className="Home">
+                    <div className="Titles">
+                        <h1>Bienvenido</h1>
+                        <h2> Con el software eAqua™ usted puede: </h2>
                     </div>
-                </div>
+
+                    <div className="HubPrincipal">
+                        
+                        <div className="HubSecundario">
+                            <img src="/img/alumno.png" alt="alumno" ></img>
+                            <p> Administrar la información de los alumnos </p>
+
+                        </div>
+
+                        <div className="HubSecundario">
+                            <img src="/img/instructor.png" alt="instructor" ></img>
+                            <p> Administrar la información de los instructores</p>
+
+                        </div>
+                        
+                        <div className="HubSecundario">
+                            <img src="/img/clase.png" alt="clase" ></img>
+                            <p> Organizar las clases del plantel </p>
+
+                        </div>
+                        
+                        <div className="HubSecundario">
+                            <img src="/img/pago.png" alt="pago" ></img>
+                            <p> Gestionar los pagos del alumnado y la docencia</p>
+                        </div>
+
+                    </div>
             </div>
         );
     }
