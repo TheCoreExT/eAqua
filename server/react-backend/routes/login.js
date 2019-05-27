7ce761c5fe2a357b53310e22eff4b45842d451fd
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-27 16:39:18 -0500

    cambio de ruta relativa a dominio netifly

--- server/react-backend/routes/login.js
+++ server/react-backend/routes/login.js
@@ -32,10 +32,10 @@ router.post('/', function(request, response) {
 			if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
-				response.redirect('/');
+				response.redirect('https://eaqua.netlify.com/');
 			} else {
                 response.send();
-				response.redirect('/');
+				response.redirect('https://eaqua.netlify.com/');
 			}			
 			response.end();
 		});
@@ -48,7 +48,7 @@ router.post('/logout', function(request, response) {
 	
 	request.session.loggedin = false;
 	request.session.username = "";
-	response.redirect('/');
+	response.redirect('https://eaqua.netlify.com/');
 			
 });
 
