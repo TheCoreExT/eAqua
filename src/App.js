29f5ecc5ac4112f4d6a36bfbfe8a60a115757d9b
Diego Rosas <de.rosasgonzalez@ugto.mx>
2019-05-28 00:28:20 -0500

    true states

--- src/App.js
+++ src/App.js
@@ -27,10 +27,11 @@ export default class App extends Component {
   componentDidMount() {
     fetch('https://157.230.165.99:3001/login')
     .then(res => res.json())
-    .then(loggedin => this.setState({ loggedin }));
+    .then(this.setState({ loggedin: true }));
 
   }
     componentDidUpdate() {
+      this.setState({loggedin: true});
       fetch('https://157.230.165.99:3001/login')
       .then(res => res.json())
       .then(loggedin => this.setState({ loggedin }));
